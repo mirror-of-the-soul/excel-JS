7482ee34ebd4227bc2db68fc7e655c5c06af8ab3
mirror-of-the-soul <grigar19@gmail.com>
2021-01-07 17:54:49 +0300

    create logic toolbar

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,5 +1,6 @@
 import {$} from '@core/dom';
 import {Emmiter} from '@core/Emmiter';
+import {StoreSubscriber} from '@core/StoreSubscriber.JS';
 
 export class Excel {
   constructor(selector, options) {
@@ -7,6 +8,7 @@ export class Excel {
     this.components = options.components || []
     this.store = options.store
     this.emitter = new Emmiter()
+    this.subscriber = new StoreSubscriber(this.store)
   }
 
   getRoot() {
@@ -35,9 +37,11 @@ export class Excel {
     // 2 способ через node
     this.$el.append(this.getRoot())
 
+    this.subscriber.subscribeComponents(this.components)
     this.components.forEach(component => component.init())
   }
   destroy() {
+    this.subscriber.unsubscribeFromStore()
     this.components.forEach(component => component.destroy())
   }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,9 +5,9 @@ export class ExcelComponent extends DomListener {
     super($root, options.listeners);
     this.name = options.name || ''
     this.emitter = options.emitter
+    this.subscribe = options.subscribe || []
     this.store = options.store
     this.unsubscribers = []
-    this.storeSub = null
 
     this.prepare()
   }
@@ -33,9 +33,11 @@ export class ExcelComponent extends DomListener {
   $dispatch(action) {
     this.store.dispatch(action)
   }
+  // в этот метод приходя только те изменения по тем полям, на которые мы подписались
+  storeChanged() {}
 
-  $subscribe(fn) {
-    this.storeSub = this.store.subscribe(fn)
+  isWatching(key) {
+    return this.subscribe.includes(key)
   }
 
   // инициализируем компонент
@@ -49,6 +51,5 @@ export class ExcelComponent extends DomListener {
   destroy() {
     this.removeDomListeners()
     this.unsubscribers.forEach(unsub => unsub())
-    this.storeSub.unsubscribers()
   }
 }
